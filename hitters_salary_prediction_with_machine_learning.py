d8a5dbe70185b54e1fd821c6450ecc4e64a3470c
emreoz <75579555+osmanemreoz@users.noreply.github.com>
2022-05-10 19:20:12 +0300

    Add files via upload

---
+++ hitters_salary_prediction_with_machine_learning.py
@@ -0,0 +1,441 @@
+############################################
+# Gerekli Kütüphane ve Fonksiyonlar
+############################################
+
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+pd.set_option('display.float_format', lambda x: '%.2f' % x)
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import StandardScaler
+
+df = pd.read_csv("7. Hafta Makine Öğrenmesi/ödev/Maaş Tahmin Modeli Oluşturma/hitters.csv")
+
+
+
+def check_df(dataframe, head=5):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Head #####################")
+    print(dataframe.head(head))
+    print("##################### Tail #####################")
+    print(dataframe.tail(head))
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+    print("##################### Quantiles #####################")
+    print(dataframe.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
+check_df(df)
+
+
+def grab_col_names(df, cat_th=10, car_th=20):
+    """
+
+    Veri setindeki kategorik, numerik ve kategorik fakat kardinal değişkenlerin isimlerini verir.
+    Not: Kategorik değişkenlerin içerisine numerik görünümlü kategorik değişkenler de dahildir.
+
+    Parameters
+    ------
+        df: df
+                Değişken isimleri alınmak istenilen df
+        cat_th: int, optional
+                numerik fakat kategorik olan değişkenler için sınıf eşik değeri
+        car_th: int, optinal
+                kategorik fakat kardinal değişkenler için sınıf eşik değeri
+
+    Returns
+    ------
+        cat_cols: list
+                Kategorik değişken listesi
+        num_cols: list
+                Numerik değişken listesi
+        cat_but_car: list
+                Kategorik görünümlü kardinal değişken listesi
+
+    Examples
+    ------
+        import seaborn as sns
+        df = sns.load_dataset("iris")
+        print(grab_col_names(df))
+
+
+    Notes
+    ------
+        cat_cols + num_cols + cat_but_car = toplam değişken sayısı
+        num_but_cat cat_cols'un içerisinde.
+        Return olan 3 liste toplamı toplam değişken sayısına eşittir: cat_cols + num_cols + cat_but_car = değişken sayısı
+
+    """
+
+
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in df.columns if df[col].dtypes == "O"]
+    num_but_cat = [col for col in df.columns if df[col].nunique() < cat_th and
+                   df[col].dtypes != "O"]
+    cat_but_car = [col for col in df.columns if df[col].nunique() > car_th and
+                   df[col].dtypes == "O"]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    # num_cols
+    num_cols = [col for col in df.columns if df[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {df.shape[0]}")
+    print(f"Variables: {df.shape[1]}")
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+    return cat_cols, num_cols, cat_but_car
+
+
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+
+
+
+#############################################
+# GELİŞMİŞ FONKSİYONEL KEŞİFÇİ VERİ ANALİZİ (ADVANCED FUNCTIONAL EDA)
+#############################################
+
+# 1. Outliers (Aykırı Değerler)
+# 2. Missing Values (Eksik Değerler)
+# 3. Feature Extraction (Özellik Çıkarımı)
+# 4. Encoding (Label Encoding, One-Hot Encoding, Rare Encoding)
+# 5. Feature Scaling (Özellik Ölçeklendirme)
+
+
+#############################################
+# 1. Outliers (Aykırı Değerler)
+#############################################
+
+def outlier_thresholds(dataframe, col_name, q1=0.25, q3=0.75):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+
+def check_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
+    else:
+        return False
+
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+
+for col in num_cols:
+    print(col, check_outlier(df, col))
+
+for col in num_cols:
+    if check_outlier(df, col):
+        replace_with_thresholds(df, col)
+
+for col in num_cols:
+    print(col, check_outlier(df, col))
+
+
+#############################################
+# 2. Kategorik Değişken Analizi (Analysis of Categorical Variables)
+#############################################
+
+def cat_summary(dataframe, col_name, plot=False):
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+    print("##########################################")
+
+    if plot:
+        sns.countplot(x=dataframe[col_name], data=dataframe)
+        plt.show()
+
+
+for col in cat_cols:
+    cat_summary(df, col, plot=True)
+
+
+#############################################
+# 3. Sayısal Değişken Analizi (Analysis of Numerical Variables)
+#############################################
+
+def num_summary(dataframe, numerical_col, plot=False):
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
+
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+    if plot:
+        dataframe[numerical_col].hist(bins=20)
+        plt.xlabel(numerical_col)
+        plt.title(numerical_col)
+        plt.show()
+
+
+for col in num_cols:
+    num_summary(df, col, plot=True)
+
+
+#############################################
+# 4. Hedef Değişken Analizi (Analysis of Target Variable)
+#############################################
+
+def target_summary_with_cat(dataframe, target, categorical_col):
+    print(pd.DataFrame({"TARGET_MEAN": dataframe.groupby(categorical_col)[target].mean()}), end="\n\n\n")
+
+
+for col in cat_cols:
+    target_summary_with_cat(df, "Salary", col)
+
+
+#############################################
+# 5. Korelasyon Analizi (Analysis of Correlation)
+#############################################
+
+def high_correlated_cols(dataframe, plot=False, corr_th=0.90):
+    corr = dataframe.corr()
+    cor_matrix = corr.abs()
+    upper_triangle_matrix = cor_matrix.where(np.triu(np.ones(cor_matrix.shape), k=1).astype(np.bool))
+    drop_list = [col for col in upper_triangle_matrix.columns if any(upper_triangle_matrix[col] > corr_th)]
+    if plot:
+        import seaborn as sns
+        import matplotlib.pyplot as plt
+        sns.set(rc={'figure.figsize': (15, 15)})
+        sns.heatmap(corr, cmap="RdBu")
+        plt.show()
+    return drop_list
+
+
+high_correlated_cols(df, plot=True)
+
+
+
+
+df.dropna(inplace=True)
+
+#############################################
+# 3. Feature Extraction (Özellik Çıkarımı)
+#############################################
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+new_num_cols = [col for col in num_cols if col not in ["Salary", "Years"]]
+
+df[new_num_cols] = df[new_num_cols] + 1
+
+
+# 1. Feature Engineering (Değişken Mühendisliği)
+df.columns = [col.upper() for col in df.columns]
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+df.describe().T
+# RATIO OF VARIABLES
+
+# CAREER RUNS RATIO
+df["NEW_C_RUNS_RATIO"] = df["RUNS"] / df["CRUNS"]
+# CAREER BAT RATIO
+df["NEW_C_ATBAT_RATIO"] = df["ATBAT"] / df["CATBAT"]
+# CAREER HITS RATIO
+df["NEW_C_HITS_RATIO"] = df["HITS"] / df["CHITS"]
+# CAREER HMRUN RATIO
+df["NEW_C_HMRUN_RATIO"] = df["HMRUN"] / df["CHMRUN"]
+# CAREER RBI RATIO
+df["NEW_C_RBI_RATIO"] = df["RBI"] / df["CRBI"]
+# CAREER WALKS RATIO
+df["NEW_C_WALKS_RATIO"] = df["WALKS"] / df["CWALKS"]
+df["NEW_C_HIT_RATE"] = df["CHITS"] / df["CATBAT"]
+# PLAYER TYPE : RUNNER
+df["NEW_C_RUNNER"] = df["CRBI"] / df["CHITS"]
+# PLAYER TYPE : HIT AND RUN
+df["NEW_C_HIT-AND-RUN"] = df["CRUNS"] / df["CHITS"]
+# MOST VALUABLE HIT RATIO IN HITS
+df["NEW_C_HMHITS_RATIO"] = df["CHMRUN"] / df["CHITS"]
+# MOST VALUABLE HIT RATIO IN ALL SHOTS
+df["NEW_C_HMATBAT_RATIO"] = df["CATBAT"] / df["CHMRUN"]
+
+#Annual Averages
+df["NEW_CATBAT_MEAN"] = df["CATBAT"] / df["YEARS"]
+df["NEW_CHITS_MEAN"] = df["CHITS"] / df["YEARS"]
+df["NEW_CHMRUN_MEAN"] = df["CHMRUN"] / df["YEARS"]
+df["NEW_CRUNS_MEAN"] = df["CRUNS"] / df["YEARS"]
+df["NEW_CRBI_MEAN"] = df["CRBI"] / df["YEARS"]
+df["NEW_CWALKS_MEAN"] = df["CWALKS"] / df["YEARS"]
+
+
+# PLAYER LEVEL
+df.loc[(df["YEARS"] <= 2), "NEW_YEARS_LEVEL"] = "Junior"
+df.loc[(df["YEARS"] > 2) & (df['YEARS'] <= 5), "NEW_YEARS_LEVEL"] = "Mid"
+df.loc[(df["YEARS"] > 5) & (df['YEARS'] <= 10), "NEW_YEARS_LEVEL"] = "Senior"
+df.loc[(df["YEARS"] > 10), "NEW_YEARS_LEVEL"] = "Expert"
+
+
+# PLAYER LEVEL X DIVISION
+
+df.loc[ (df["NEW_YEARS_LEVEL"] == "Junior") & (df["DIVISION"] == "E"), 'NEW_DIV_CAT'] = "Junior-East"
+df.loc[(df["NEW_YEARS_LEVEL"] == "Junior") & (df["DIVISION"] == "W"), 'NEW_DIV_CAT'] = "Junior-West"
+df.loc[(df["NEW_YEARS_LEVEL"] == "Mid") & (df["DIVISION"] == "E"), 'NEW_DIV_CAT'] = "Mid-East"
+df.loc[(df["NEW_YEARS_LEVEL"] == "Mid") & (df["DIVISION"] == "W"), 'NEW_DIV_CAT'] = "Mid-West"
+df.loc[(df["NEW_YEARS_LEVEL"] == "Senior") & (df["DIVISION"] == "E"), 'NEW_DIV_CAT'] = "Senior-East"
+df.loc[(df["NEW_YEARS_LEVEL"] == "Senior") & (df["DIVISION"] == "W"), 'NEW_DIV_CAT'] = "Senior-West"
+df.loc[(df["NEW_YEARS_LEVEL"] == "Expert") & (df["DIVISION"] == "E"), 'NEW_DIV_CAT'] = "Expert-East"
+df.loc[(df["NEW_YEARS_LEVEL"] == "Expert") & (df["DIVISION"] == "W"), 'NEW_DIV_CAT'] = "Expert-West"
+
+# Player Promotion to Next League
+df.loc[(df["LEAGUE"] == "N") & (df["NEWLEAGUE"] == "N"), "NEW_PLAYER_PROGRESS"] = "StandN"
+df.loc[(df["LEAGUE"] == "A") & (df["NEWLEAGUE"] == "A"), "NEW_PLAYER_PROGRESS"] = "StandA"
+df.loc[(df["LEAGUE"] == "N") & (df["NEWLEAGUE"] == "A"), "NEW_PLAYER_PROGRESS"] = "Descend"
+df.loc[(df["LEAGUE"] == "A") & (df["NEWLEAGUE"] == "N"), "NEW_PLAYER_PROGRESS"] = "Ascend"
+
+
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+
+
+# Label Encoding
+def label_encoder(dataframe, binary_col):
+    labelencoder = LabelEncoder()
+    dataframe[binary_col] = labelencoder.fit_transform(dataframe[binary_col])
+    return dataframe
+
+binary_cols = [col for col in df.columns if
+               df[col].dtype not in [int, float] and df[col].nunique() == 2]
+
+for col in binary_cols:
+    df = label_encoder(df, col)
+
+
+
+
+# Rare Encoding
+def rare_analyser(dataframe, target, cat_cols):
+    for col in cat_cols:
+        print(col, ":", len(dataframe[col].value_counts()))
+        print(pd.DataFrame({"COUNT": dataframe[col].value_counts(),
+                            "RATIO": dataframe[col].value_counts() / len(dataframe),
+                            "TARGET_MEAN": dataframe.groupby(col)[target].mean()}), end="\n\n\n")
+
+
+
+def rare_encoder(dataframe, rare_perc, cat_cols):
+    temp_df = dataframe.copy()
+    rare_columns = [col for col in cat_cols if (temp_df[col].value_counts() / len(temp_df) < rare_perc).sum() > 1]
+
+    for col in rare_columns:
+        tmp = temp_df[col].value_counts() / len(temp_df)
+        rare_labels = tmp[tmp < rare_perc].index
+        temp_df[col] = np.where(temp_df[col].isin(rare_labels), 'Rare', temp_df[col])
+
+    return temp_df
+
+rare_analyser(df,"SALARY", cat_cols)
+df = rare_encoder(df, 0.01, cat_cols)
+
+# 6. One-Hot Encoding
+
+def one_hot_encoder(dataframe, categorical_cols, drop_first=False):
+    dataframe = pd.get_dummies(dataframe, columns=categorical_cols, drop_first=drop_first)
+    return dataframe
+
+ohe_cols = [col for col in df.columns if 10 >= df[col].nunique() > 2]
+df = one_hot_encoder(df, ohe_cols, drop_first=True)
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+num_cols.remove("SALARY")
+
+# 7. Robust-Scaler
+for col in num_cols:
+    transformer = RobustScaler().fit(df[[col]])
+    df[col] = transformer.transform(df[[col]])
+
+
+
+
+
+
+
+######################################################
+# Multiple Linear Regression
+######################################################
+X = df.drop("SALARY", axis=1)
+y = df[["SALARY"]]
+
+##########################
+# Model
+##########################
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.30, random_state=1)
+
+reg_model = LinearRegression()
+reg_model.fit(X_train, y_train)
+
+# sabit (b - bias)
+reg_model.intercept_
+# array([677.17569746])
+
+# coefficients (w - weights)
+reg_model.coef_
+# array([[   51.94177313,    75.59220098,   -22.52978942,   -92.40160036,
+#           168.13468272,    53.99857806,  -291.25339737,   -64.7930076 ,
+#           239.7843044 ,   102.02122164,   -66.94945381,     6.90615146,
+#            92.99919502,   -60.55744376,   -39.10944254,    51.48375148,
+#           -86.09991475,    86.78593739,    81.66341941,  -117.02468997,
+#           912.70764461,  -820.41937828,    37.23649264,  -311.05794694,
+#           -35.23348024,  -133.93316717,   -24.78754548,   -16.76899732,
+#           -31.64107928,    24.7915167 , -1620.75988251,  1227.62343374,
+#            54.81355735,   272.52974588,    46.06753949,    -9.60735276,
+#            48.32523535,  -102.29406462,     2.66113677,   -12.46392982,
+#            24.34723013,    23.97800522,   -67.13925238,   -35.15481223,
+#            18.12984247,   -15.4687057 ,   -10.29183365,   -71.37158577,
+#           -50.26561011]])
+
+
+##########################
+# Tahmin
+##########################
+
+
+##########################
+# Tahmin Başarısını Değerlendirme
+##########################
+
+# Train RMSE
+y_pred = reg_model.predict(X_train)
+np.sqrt(mean_squared_error(y_train, y_pred))
+# 174.6466855198905
+
+# TRAIN RKARE
+reg_model.score(X_train, y_train)
+# 0.805120210966488
+
+
+# Test RMSE
+y_pred = reg_model.predict(X_test)
+np.sqrt(mean_squared_error(y_test, y_pred))
+# 296.37796345758045
+
+# Test RKARE
+reg_model.score(X_test, y_test)
+# 0.4783398431251118
+
+# 10 Katlı CV RMSE
+np.mean(np.sqrt(-cross_val_score(reg_model,
+                                 X, y,
+                                 cv=10,
+                                 scoring="neg_mean_squared_error")))
+# 256.2987991302137
